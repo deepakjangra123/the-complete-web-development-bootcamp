9e6bbb6e88367b626435a3f3e66bb77702d16c18
DEEPAK JANGRA <89759969+deepakjangra123@users.noreply.github.com>
2023-09-15 16:40:51 +0530

    Add files via upload

---
+++ node.js
@@ -0,0 +1,28 @@
+//to get into repl
+
+// type node in cli
+
+// to get out of repl 
+// ctrl + c  twice 
+
+// ororororo
+// .exit
+
+
+// clear to everythng
+
+
+/// to run a node file 
+
+// goto terminal 
+
+// open file folder 
+//type node file_name.js
+
+
+
+// to initialize npm
+//type npm init in cli
+
+//then press enter afeter namaing it
+
